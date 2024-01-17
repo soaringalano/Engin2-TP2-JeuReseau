411bccf60ac8fdc00548ba11fb8d007d4b2f6e9e
Axel Cobelo Weisz <axelcobelo@yahoo.com>
2024-01-17 12:06:21 -0500

    Equipe01 Player UI implemented
    code de triche de gain et perte d'energie implementés aussi

---
+++ Assets/E01playerUImanager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class E01playerUImanager : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Update()
+    {
+        // Increment the variable when the 'i' key is pressed
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+           slider.value++;
+      
+        }
+
+        // Decrement the variable when the 'o' key is pressed
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            slider.value--;
+        }
+    }
+
+}
