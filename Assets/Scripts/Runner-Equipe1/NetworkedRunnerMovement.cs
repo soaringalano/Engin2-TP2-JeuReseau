ca17b4eda0d54e6a39db45361175297d81523e55
rallardm@gmail.com <rallardm@gmail.com>
2024-01-25 08:46:58 -0500

    Adding a global scene for the teams game play test

--- Assets/Scripts/Runner-Equipe1/NetworkedRunnerMovement.cs
+++ Assets/Scripts/Runner-Equipe1/NetworkedRunnerMovement.cs
@@ -58,16 +58,6 @@ public class NetworkedRunnerMovement : NetworkBehaviour
 
     private void FixedUpdate()
     {
-
-        //if (m_floorTrigger.IsOnFloor)
-        //{
-        //    SetDirectionalInputs();
-        //    SetRunningInput();
-        //    UpdateMovementsToAnimator();
-        //    RotatePlayerMesh();
-        //    ApplyMovementsOnFloorFU();
-        //}
-
         if (!isLocalPlayer)
         {
             return;
