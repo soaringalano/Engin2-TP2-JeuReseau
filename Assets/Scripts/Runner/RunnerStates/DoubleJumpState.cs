ea74f92e22aceb4c73c544e1fe2c321cf5271cb3
rallardm@gmail.com <rallardm@gmail.com>
2024-02-04 04:35:29 -0500

    Adding the main runner states

---
+++ Assets/Scripts/Runner/RunnerStates/DoubleJumpState.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpState : RunnerState
+{
+
+    public override bool CanEnter(IState currentState)
+    {
+        // if current stamina does not support this action, then don't allow to enter
+        if (m_stateMachine.MustRest(m_stateMachine.StaminaLoseSpeedInDoubleJump))
+        {
+            return false;
+        }
+        // if current in JumpState and not on the ground and is_jumping
+        if (currentState.GetType() == typeof(JumpState) &&
+            m_stateMachine.m_floorTrigger.IsOnFloor == false &&
+            m_stateMachine.m_isJumping == true)
+        {
+            // if space bar pressed, then enter
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                return true;
+            }
+        }
+        // otherwise, refuse to enter
+        return false;
+    }
+
+    public override bool CanExit()
+    {
+        // if must rest, then allow to exit
+        if (m_stateMachine.MustRest(m_stateMachine.StaminaLoseSpeedInDoubleJump))
+        {
+            return true;
+        }
+        // if is now on floor
+        if(m_stateMachine.m_floorTrigger.IsOnFloor)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override void OnEnter()
+    {
+        Debug.Log("Enter state: DoubleJumpState\n");
+        m_stateMachine.DoubleJump();
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exit state: DoubleJumpState\n");
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        m_stateMachine.FixedLoseStamina(m_stateMachine.StaminaLoseSpeedInDoubleJump);
+    }
+
+}
---
+++ Assets/Scripts/Runner/RunnerStates/RunState.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunState : RunnerState
+{
+
+    public override bool CanEnter(IState currentState)
+    {
+        if (m_stateMachine.MustRest(m_stateMachine.StaminaLoseSpeedInRun))
+        {
+            return false;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override bool CanExit()
+    {
+        if (m_stateMachine.MustRest(m_stateMachine.StaminaLoseSpeedInRun))
+        {
+            return true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.Space))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override void OnEnter()
+    {
+        Debug.Log("Enter state: RunState\n");
+        m_stateMachine.SetRunningInput();
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exit state: RunState\n");
+
+    }
+
+    public override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        m_stateMachine.FixedLoseStamina(m_stateMachine.StaminaLoseSpeedInRun);
+        m_stateMachine.UpdateMovementsToAnimator();
+        m_stateMachine.ApplyMovementsOnFloorFU();
+        
+    }
+
+}
