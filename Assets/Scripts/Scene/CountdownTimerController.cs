aa19691b8be9a68fa7ee3b469861b68a3d926378
soaringalano <linmao78730@gmail.com>
2024-02-07 14:32:43 -0500

    Add CountdownTimer prefab and script to hunter and runner assets

---
+++ Assets/Scripts/Scene/CountdownTimerController.cs
@@ -0,0 +1,53 @@
+﻿using Mirror;
+using Runhunt.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class CountdownTimerController : NetworkBehaviour
+{
+
+    [SyncVar]
+    public float m_currentTimeInSecond = 180;
+
+    [field: SerializeField] public GameObject m_countdownTimerTextMesh { get; set; }
+
+    void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!isServer)
+        {
+            return;
+        }*/
+        string timetext = GameObjectHelper.GetTimeAsString(m_currentTimeInSecond);
+        m_countdownTimerTextMesh.GetComponent<TextMeshProUGUI>().SetText(timetext);
+    }
+
+    void FixedUpdate()
+    {
+        /*if (!isServer)
+        { 
+            return; 
+        }*/
+
+        if(m_currentTimeInSecond >  0)
+            m_currentTimeInSecond -= Time.fixedDeltaTime;
+    }
+
+    public bool TimesUp()
+    {
+        return m_currentTimeInSecond <= 0;
+    }
+
+}
