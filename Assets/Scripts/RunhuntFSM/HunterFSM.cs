748a6847297b46d8608de00d3d52350ee9854a02
Axel Cobelo Weisz <axelcobelo@yahoo.com>
2024-02-16 15:03:34 -0500

    Hunter Speed, shift speed, and mouse/cam rotation speed corrected
    Hunter speed and shift speed corrected within the code,  the cam/mouse rotation speed corrected in the parameter of the cinemachine in the prefab

--- Assets/Scripts/RunhuntFSM/HunterFSM.cs
+++ Assets/Scripts/RunhuntFSM/HunterFSM.cs
@@ -31,8 +31,8 @@ namespace Mirror
         public Transform HunterSelectionPose { get; private set; }
 
         [field: Header("HunterLookAtFloorBody controls Settings")]
-        private float FloorBodyMinSpeed { get; set; } = 80.0f;
-        private float FloorBodyMaxSpeed { get; set; } = 150.0f;
+        private float FloorBodyMinSpeed { get; set; } = 120.0f;
+        private float FloorBodyMaxSpeed { get; set; } = 280.0f;
         private float FloorBodyCurrentMaxSpeed { get; set; } = 80f;
         private bool IsFloorBodySetToStop { get; set; } = false;
         private float FloorBodyDecelerationRate { get; set; } = 0.2f;
@@ -40,7 +40,7 @@ namespace Mirror
 
 
         [field: Header("Scrolling Settings")]
-        private float ScrollSpeed { get; set; } = 200.0f;
+        [field: SerializeField] private float ScrollSpeed { get; set; } = 25.0f;
         private float MinCamDist { get; set; } = 30.0f;
         private float MaxCamDist { get; set; } = 80.0f;
         private float MinCamFOV { get; set; } = 1.0f;
@@ -50,7 +50,7 @@ namespace Mirror
 
         [field: Header("Rotating PLatform Settings")]
         public GameObject TerrainPlane { get; set; }
-        private float RotationSpeed { get; set; } = 0.01f;
+        [field: SerializeField]  private float RotationSpeed { get; set; } = 0.01f;
         private float MaxRotationAngle { get; set; } = 5f;
         public Vector3 PreviousMousePosition { get; set; }
         private Vector3 m_currentRotation = Vector3.zero;
@@ -255,7 +255,6 @@ namespace Mirror
         public void LateUpdateStopHunterLookAtFloorBodyRBVelocity()
         {
             if (!IsFloorBodySetToStop) return;
-
             //Debug.Log("Floor Body Set To Stop");
             RB.velocity = Vector3.Lerp(RB.velocity, Vector3.zero, FloorBodyDecelerationRate);
         }
@@ -306,7 +305,7 @@ namespace Mirror
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                FloorBodyCurrentMaxSpeed = FloorBodyMaxSpeed;
+                FloorBodyCurrentMaxSpeed = FloorBodyMaxSpeed + 40;
                 return FloorBodyMaxSpeed;
             }
 
